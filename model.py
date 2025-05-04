059872441aa606875b073def9473661f2df10141
Varada Hampiholi <varada.hampiholi@fractal.ai>
2025-05-04 17:42:58 +0530

    updating model.py file

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ X = df.drop(['Reached.on.Time_Y.N', 'ID'], axis=1)
 y = df['Reached.on.Time_Y.N']
 
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Train model
 model = RandomForestClassifier()
